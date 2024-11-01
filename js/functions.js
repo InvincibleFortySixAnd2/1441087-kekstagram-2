bfbb68f93c4e4501b4650f62a25052bac4a8a01c
FortySixAnd2 <jmaxj91j@gmail.com>
2024-11-01 09:26:23 +0300

    functions,js update.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,3 @@
-/*
-Проверки после каждой функции оставлены для того,
-чтобы не было ошибок о "неиспользовании" функций
-и можно было сделать Pull Request.
-*/
-
 /**
  * Проверяет строку на соответствие допустимой длине.
  * @param {string} string - Строка для проверки.
@@ -14,15 +8,7 @@
 function checkLength (string = '', length = 1) {
   return string.length <= length;
 }
-
-
 checkLength();
-/*
-console.log(checkLength()); // true
-console.log(checkLength('проверяемая строка', 20)); // true
-console.log(checkLength('проверяемая строка', 18)); // true
-console.log(checkLength('проверяемая строка', 10)); // false
-*/
 
 
 /**
@@ -41,16 +27,20 @@ function checkPalindrome (string = '') {
 
   return normalizedString === reversedString;
 }
-
-
 checkPalindrome();
-/*
-console.log(checkPalindrome()); // true
-console.log(checkPalindrome('топот')); // true
-console.log(checkPalindrome('ДовОд')); // true
-console.log(checkPalindrome('Кекс')); // false
-console.log(checkPalindrome('Лёша на полке клопа нашёл ')); // true
-*/
+
+
+/**
+ * Проверяет является ли строка палиндромом.
+ * @param {string} string - Строка для проверки.
+ * @returns {boolean} Является ли строка палиндромом.
+ * @author spearance
+ */
+function isPalindrome (string = '') {
+  const normalizedString = string.replace(/\s+/g, '').toLocaleLowerCase();
+  return normalizedString === [...normalizedString].reverse().join('');
+}
+isPalindrome();
 
 
 /**
@@ -61,28 +51,31 @@ console.log(checkPalindrome('Лёша на полке клопа нашёл '));
  * @returns {number} Целое число, состоящее из цифр в строке.
  * @author FortySixAnd2
  */
-function getYear (string = '') {
-  let year = '';
+function getNumbers (string = '') {
+  let numbers = '';
   string = string.toString(); // Преобразуем входной атрибут в строку, чтобы функция обрабатывала и числа.
 
   for (let l = 0; l < string.length; l++) {
     const parseLetter = parseInt(string[l], 10);
     const isNumber = !Number.isNaN(parseLetter);
-    year = (isNumber) ? year += parseLetter : year;
+    numbers = (isNumber) ? numbers += parseLetter : numbers;
   }
 
-  return (parseInt(year, 10));
+  return (parseInt(numbers, 10));
 }
+getNumbers();
 
-getYear();
-/*
-console.log(getYear()); // NaN
-console.log(getYear('2023 год')); // 2023
-console.log(getYear('ECMAScript 2022')); // 2022
-console.log(getYear('1 кефир, 0.5 батона')); // 105
-console.log(getYear('агент 007')); // 7
-console.log(getYear('а я томат')); // NaN
-console.log(getYear(2023)); // 2023
-console.log(getYear(-1)); // 1
-console.log(getYear(1.5)); // 15
-*/
+
+/**
+ * Принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
+ * и возвращает их в виде целого положительного числа.
+ * Если в строке нет ни одной цифры — возвращает NaN.
+ * @param {string} string - Строка для обработки.
+ * @returns {number} Целое число, состоящее из цифр в строке.
+ * @author spearance
+ */
+function extractNumbers (string = '') {
+  string = string.toString();
+  return parseInt(string.replace(/\D+/g, ''), 10);
+}
+extractNumbers();
